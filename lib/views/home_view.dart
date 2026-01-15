ca25f915816698782511cbdba89324ae218b0318
Abdulrahman715 <apdonagip715@gmail.com>
2026-01-15 14:45:59 +0200

    update the colors

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -6,13 +6,13 @@ class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   final List<TuneModel> items = const [
-    TuneModel(color: Color(0xffFD3F37), sound: 'sounds/note1.wav'),
-    TuneModel(color: Color(0xffFC9729), sound: 'sounds/note2.wav'),
-    TuneModel(color: Color(0xffFCEA55), sound: 'sounds/note3.wav'),
-    TuneModel(color: Color(0xff32AE55), sound: 'sounds/note4.wav'),
-    TuneModel(color: Color(0xff009486), sound: 'sounds/note5.wav'),
-    TuneModel(color: Color(0xff0096EA), sound: 'sounds/note6.wav'),
-    TuneModel(color: Color(0xffA126AA), sound: 'sounds/note7.wav'),
+    TuneModel(color: Color.fromARGB(255, 143, 137, 137), sound: 'sounds/note1.wav'),
+    TuneModel(color: Color.fromARGB(255, 141, 97, 51), sound: 'sounds/note2.wav'),
+    TuneModel(color: Color.fromARGB(255, 160, 158, 145), sound: 'sounds/note3.wav'),
+    TuneModel(color: Color.fromARGB(255, 58, 85, 66), sound: 'sounds/note4.wav'),
+    TuneModel(color: Color.fromARGB(255, 117, 153, 149), sound: 'sounds/note5.wav'),
+    TuneModel(color: Color.fromARGB(255, 166, 174, 179), sound: 'sounds/note6.wav'),
+    TuneModel(color: Color.fromARGB(255, 146, 142, 146), sound: 'sounds/note7.wav'),
   ];
 
   @override
